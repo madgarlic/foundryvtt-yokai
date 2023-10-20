4f68555c29439906dc6a279e614ad1c60c4dff45
Mario <mario.daglio@me.com>
2023-10-20 08:31:07 +0200

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const YOKAI = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ YOKAI.abilities = {
+  "str": "YOKAI.AbilityStr",
+  "dex": "YOKAI.AbilityDex",
+  "con": "YOKAI.AbilityCon",
+  "int": "YOKAI.AbilityInt",
+  "wis": "YOKAI.AbilityWis",
+  "cha": "YOKAI.AbilityCha"
+};
+
+YOKAI.abilityAbbreviations = {
+  "str": "YOKAI.AbilityStrAbbr",
+  "dex": "YOKAI.AbilityDexAbbr",
+  "con": "YOKAI.AbilityConAbbr",
+  "int": "YOKAI.AbilityIntAbbr",
+  "wis": "YOKAI.AbilityWisAbbr",
+  "cha": "YOKAI.AbilityChaAbbr"
+};
