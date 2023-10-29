495ab98f9bae51c51d1e15ea4a164f5d163359fe
Mario <mario.daglio@me.com>
2023-10-29 08:20:52 +0100

    Improve comments

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -7,9 +7,11 @@ export class YokaiActor extends Actor {
   /** @override */
   prepareData() {
     // Prepare data for the actor. Calling the super version of this executes
-    // the following, in order: data reset (to clear active effects),
-    // prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
-    // prepareDerivedData().
+    // the following, in order: 
+    // - data reset (to clear active effects),
+    // - prepareBaseData(), 
+    // - prepareEmbeddedDocuments() (including active effects),
+    // - prepareDerivedData().
     super.prepareData();
   }
 
